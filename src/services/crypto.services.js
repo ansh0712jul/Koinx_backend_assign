1bde97213ad0dd95fa66430c5606142e55561f07
Ansh <anshagrawal181@gmail.com>
2025-01-11 13:52:45 +0530

    api corrected

--- src/services/crypto.services.js
+++ src/services/crypto.services.js
@@ -4,7 +4,7 @@ import Crypto from "../models/crypto.model.js";
 // fn to fetch crypto data
 const fetchCryptoData = async () =>{
     try{
-        const response = await axios.get(`${process.env.COINX_API_KEY}/coins/markets`, {
+        const response = await axios.get(`${process.env.COINX_API_KEY}`, {
             params: {
               vs_currency: 'usd',  
               ids: 'bitcoin,ethereum,matic-network',  
